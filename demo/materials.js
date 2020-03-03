0a7de6a1959d61f80b3e08f45b028cdf2a2ae9eb
iponkan <iponkan@gmail.com>
2020-03-03 16:04:27 +0800

    改变车颜色交互

--- demo/materials.js
+++ demo/materials.js
@@ -1,28 +1,17 @@
-pc.script.create('materials', function (context) {
-    var Materials = function (entity) {
-        this.entity = entity;
-    };
+var Materials = pc.createScript('materials');
 
-    Materials.prototype = {
-        initialize: function () {
-        },
+Materials.prototype.changeColor = function(color){
+    var model = this.entity.model.model;
+    if (model) {
+        var materials = model.getMaterials();
+        for (var i = 0; i < materials.length; i++) {
+            var material = materials[i];
 
-        changeColor: function (color) {
-            var model = this.entity.model.model;
-            if (model) {
-                var materials = model.getMaterials();
-                for (var i = 0; i < materials.length; i++) {
-                    var material = materials[i];
-    
-                    if (material.getName() === "body") {
-                        material.ambient = color;
-                        material.diffuse = color;
-                        material.update();
-                    }
-                }
+            if (material.getName() === "body") {
+                material.ambient = color;
+                material.diffuse = color;
+                material.update();
             }
         }
-    };
-
-    return Materials;
-});
+    }
+};
