dc598eeb972d68527ee29c810c4c47545536e483
iponkan <iponkan@gmail.com>
2020-03-03 16:14:11 +0800

    修改车身颜色

--- demo/materials.js
+++ demo/materials.js
@@ -7,7 +7,8 @@ Materials.prototype.changeColor = function(color){
         for (var i = 0; i < materials.length; i++) {
             var material = materials[i];
 
-            if (material.getName() === "body") {
+            console.log("material.name==" + material.name);
+            if (material.name === "Car_Body_A") {
                 material.ambient = color;
                 material.diffuse = color;
                 material.update();
